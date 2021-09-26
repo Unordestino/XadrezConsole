900a1cdbd54e884c4062df8e289fc9299f7bb6ee
Davi souza <hzpck17@gmail.com>
2021-09-26 08:11:55 -0300

    Fazendo os primeiros testes de movimentacao

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -5,9 +5,26 @@ using xadrez;
 namespace Xadrez_Console {
     class Program {
         static void Main(string[] args) {
-            Tabuleiro tab = new Tabuleiro(8, 8);
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
-            Tela.imprimirTabuleiro(tab);
+            try {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+                while (!partida.Terminada) {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().ToPosicão();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().ToPosicão();
+
+                    partida.executaMovimento(origem, destino);
+                }
+
+            }
+            catch (TabuleiroException e) {
+                Console.WriteLine(e.Message);
+            }
+            
         }
     }
 }
---
+++ Xadrez-Console/xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using tabuleiro;
+
+namespace xadrez {
+    class PartidaDeXadrez {
+        public Tabuleiro tab { get; private set; }
+        private int turno;
+        private Cor JogadorAtual;
+        public bool Terminada { get; private set; }
+
+        public PartidaDeXadrez() {
+            tab = new Tabuleiro(8,8);
+            turno = 1;
+            JogadorAtual = Cor.Branca;
+            Terminada = false;
+        }
+
+        public void executaMovimento(Posicao origem, Posicao destino) {
+            Peca p = tab.retirarPeca(origem);
+            p.incrementarqteMovimentos();
+            Peca pecaCapturada = tab.retirarPeca(destino);
+            tab.colocarPeca(p, destino);
+
+        }
+    }
+}
